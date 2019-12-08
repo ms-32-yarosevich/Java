16c037f8e1ce47978342ef38af8c2ecf2818280e
Andrei Yarasevich <AYarasevich@gomel.iba.by>
2019-12-08 16:12:04 +0300

    Added lab9

---
+++ lab9/src/main/java/by/gsu/ms42/util/ApiWeatherConstants.java
@@ -0,0 +1,8 @@
+package by.gsu.ms42.util;
+
+public class ApiWeatherConstants {
+
+    public static final String URL_PATH = "http://api.openweathermap.org/data/2.5/";
+    public static final String CITY_NAME = "Minsk,by";
+    public static final String API_KEY = "de3b68e08a92bf6216d8bb921d0cbe81";
+}
